ca576c86a3d5da5babcf8a6140faa23d42fe8e92
Raita Watanabe <watanaberit20171129@gmail.com>
2019-12-07 21:25:24 +0900

    add TestUploadError

--- main_test.go
+++ main_test.go
@@ -35,3 +35,23 @@ func TestUpload(t *testing.T) {
 		}
 	}
 }
+
+func TestUploadError(t *testing.T) {
+	s3Ctr := CreateS3Controller(Region, Endpoint, Profile)
+
+	tests := []struct {
+		bucket     string
+		bucketPath string
+		filePath   string
+	}{
+		{"xxxxxx", "hello.txt", "sample/hello.txt"},                              //non-existent bucket
+		{"go-localstack-github-actions-sample", "workflow.txt", "sample/xxxxxx"}, //non-existent file
+	}
+
+	for _, tt := range tests {
+		err := s3Ctr.UploadFile(tt.bucket, tt.bucketPath, tt.filePath)
+		if err == nil {
+			t.Fatal("No error was returned")
+		}
+	}
+}
