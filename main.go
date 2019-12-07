9706d4ec705199edd6b710b5628292d84652cc9c
Raita Watanabe <watanaberit20171129@gmail.com>
2019-12-07 15:56:57 +0900

    fix main.go

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-// CreateSessionForLocalstack Create a session for localstack
-func CreateSessionForLocalstack(region string, endpoint string, profile string) *session.Session {
+type S3Controller struct {
+	S3 *s3.S3
+}
+
+// createSessionForLocalstack Create a session for localstack
+func createSessionForLocalstack(region string, endpoint string, profile string) *session.Session {
 	conf := aws.Config{
 		Region:           aws.String(region),
 		Endpoint:         aws.String(endpoint),
@@ -24,31 +27,61 @@ func CreateSessionForLocalstack(region string, endpoint string, profile string) 
 	return sess
 }
 
-func main() {
-	region := "ap-northeast-1"
-	endpoint := "http://localstack:4572"
-	profile := "localstack"
-	bucket := "sample"
-
-	sess := CreateSessionForLocalstack(
+func CreateS3Controller(region string, endpoint string, profile string) S3Controller {
+	sess := createSessionForLocalstack(
 		region,
 		endpoint,
 		profile,
 	)
 
-	svc := s3.New(sess)
-
-	_, err := svc.CreateBucket(&s3.CreateBucketInput{
-		Bucket: aws.String(bucket),
-	})
-	if err != nil {
-		log.Fatal(err)
+	s3Ctr := S3Controller{
+		S3: s3.New(sess),
 	}
 
-	result, _ := svc.ListBuckets(nil)
+	return s3Ctr
+}
+
+func (s3Ctr *S3Controller) CreateBuckets(buckets []string) error {
+	for _, b := range buckets {
+		_, err := s3Ctr.S3.CreateBucket(&s3.CreateBucketInput{
+			Bucket: aws.String(b),
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s3Ctr *S3Controller) ListBuckets() error {
+	result, err := s3Ctr.S3.ListBuckets(nil)
+	if err != nil {
+		return err
+	}
 	fmt.Println("Buckets:")
 
 	for _, b := range result.Buckets {
 		fmt.Printf("* %s created on %s\n", aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
 	}
+
+	return nil
+}
+
+func main() {
+	fmt.Printf("")
+	region := "ap-northeast-1"
+	endpoint := "http://localstack:4572"
+	profile := "localstack"
+	buckets := []string{
+		"sample1",
+		"sample2",
+		"sample3",
+	}
+
+	s3Ctr := CreateS3Controller(region, endpoint, profile)
+
+	s3Ctr.CreateBuckets(buckets)
+	s3Ctr.ListBuckets()
+
 }
