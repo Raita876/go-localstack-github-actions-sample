c33bf02970205cc4a292b11e5b6e186551880f4f
Raita Watanabe <watanaberit20171129@gmail.com>
2019-12-07 20:49:08 +0900

    add UploadFile

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -9,9 +10,11 @@ import (
 )
 
 const (
-	Region   = "ap-northeast-1"
-	Endpoint = "http://localstack:4572"
-	Profile  = "localstack"
+	Region         = "ap-northeast-1"
+	Endpoint       = "http://localstack:4572"
+	Profile        = "localstack"
+	Bucket         = "go-localstack-github-actions-sample"
+	ReadmeFilePath = "README.md"
 )
 
 // S3Controller operate s3
@@ -78,10 +81,31 @@ func (s3Ctr *S3Controller) ListBuckets() error {
 	return nil
 }
 
+// UploadFile upload file to s3 bucket
+func (s3Ctr *S3Controller) UploadFile(bucket string, bucketPath string, filePath string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = s3Ctr.S3.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(bucketPath),
+		Body:   file,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
 	s3Ctr := CreateS3Controller(Region, Endpoint, Profile)
 
 	buckets := []string{
+		Bucket,
 		"sample1",
 		"sample2",
 		"sample3",
---
+++ main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	s3Ctr := CreateS3Controller(Region, Endpoint, Profile)
+	buckets := []string{
+		Bucket,
+	}
+
+	s3Ctr.CreateBuckets(buckets)
+	result := m.Run()
+	os.Exit(result)
+}
+
+func TestUpload(t *testing.T) {
+	s3Ctr := CreateS3Controller(Region, Endpoint, Profile)
+
+	tests := []struct {
+		bucket     string
+		bucketPath string
+		filePath   string
+	}{
+		{"sample", "go-localstack-github-actions-sample/hello.txt", "sample/hello.txt"},
+		{"sample", "go-localstack-github-actions-sample/workflow.txt", "sample/workflow.txt"},
+	}
+
+	for _, tt := range tests {
+		err := s3Ctr.UploadFile(tt.bucket, tt.bucketPath, tt.filePath)
+		if err != nil {
+			t.Fatalf("Failed to Upload: %s\n", err.Error())
+		}
+	}
+}
